280254f751bfe45e7a6ffeaac1ea4887081cc398
decidev22 <hamento22@gmail.com>
2025-03-13 23:41:04 +1100

    back to esm

--- code/api-github/controller/getGithubActivityController.ts
+++ code/api-github/controller/getGithubActivityController.ts
@@ -3,6 +3,7 @@ import { getGhActivities } from "api-github/service/getGithubActivities.ts";
 
 export const getGithubActivityController = async (req: express.Request, res: express.Response) => {
   try {
+    console.log("controller is being called");
     const ghLog = await getGhActivities();
     res.status(200).send(ghLog);
   } catch (error) {
